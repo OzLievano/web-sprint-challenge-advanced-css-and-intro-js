4aa0e1534d8fb89a7f187d63423aeee3ca02daf9
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-07-31 22:01:18 -0400

    compeleted task 3 javascript

--- index.js
+++ index.js
@@ -225,10 +225,11 @@ console.log(artists[8]);
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array, index) {
-    /* code here */
-  }
-  
+function getArtistByIndex(array,index){
+  return `The artist at index ${index} is ${array[index].name}`
+}
+
+console.log(getArtistByIndex(artists,0));
   /**
 
 
